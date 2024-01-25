24f81b46825063d46d46ccc189fce395e8abb74d
Mathieu Desjarlais <github@desmat.ca>
2024-01-25 11:56:48 -0500

    cleanup

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
     //     serverActions: false,
     // },
     env: {
-        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        // FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        // FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        // FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        // FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        // FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        // FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
         AUTH_PRIVATE_KEY: process.env.AUTH_PRIVATE_KEY,
         AUTH_PUBLIC_KEY: process.env.AUTH_PUBLIC_KEY,
     }
