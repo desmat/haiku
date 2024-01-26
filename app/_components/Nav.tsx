4cca32a4f1cf02830b474f201c5f6dc776a35bcf
Mathieu Desjarlais <github@desmat.ca>
2024-01-26 10:10:55 -0500

    minor visual polish on logo

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -19,7 +19,7 @@ export function Logo({ href, onClick }: { href?: string, onClick?: any }) {
       href={href || "#"}
       className="hover:no-underline"
     >
-      <span className={font.architects_daughter.className}>h<span className={`${font.inter.className} tracking-[-2px] text-[18pt] md:text-[24pt] font-semibold`}>AI</span>kudle</span>
+      <span className={font.architects_daughter.className}>h<span className={`${font.inter.className} tracking-[-1px] text-[18pt] md:text-[24pt] font-semibold`}>AI</span>kudle</span>
     </Link>
   )
 }
