c1716da638c5e2b900a34c135663886b6fedd7b1
Mathieu Desjarlais <github@desmat.ca>
2024-01-30 14:17:43 -0500

    alert polish

--- app/_components/Alert.tsx
+++ app/_components/Alert.tsx
@@ -85,25 +85,33 @@ function TypedAlert({
 
   return (
     <div className={`_border-[1px] ${colorClasses[5]} border-solid fixed bottom-3 left-3 md:left-[calc(50vw-(700px/2))] lg:left-[calc(50vw-((700px-8rem)/2))] ${closed ? "_-z-10" : "z-20"}`}>
-      <div className={`${closed ? "opacity-0" : "opacity-100"} transition-all rounded-md ${colorClasses[0]} p-4 w-[calc(100vw-1.5rem)] md:w-[700px] shadow-md hover:shadow-lg`}>
-        <div className="flex items-center ">
-          <div className="flex-shrink-0">
-            {icon}
-          </div>
-          <div className={`${icon ? "ml-3" : ""}`}>
-            <p className={`text-sm font-medium ${colorClasses[3]}`} dangerouslySetInnerHTML={{ __html: message }} />
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="absolute top-0 right-0">
-              <button
-                type="button"
-                className={`inline-flex rounded-md ${colorClasses[0]} p-1.5 ${colorClasses[4]} ${colorClasses[1]} focus:outline-none ${colorClasses[2]} focus:ring-offset-2`}
-                onClick={handleClose}
-              >
-                <span className="sr-only">Dismiss</span>
-                <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
+      <div className={`${closed ? "opacity-0" : "opacity-100"} transition-all rounded-sm ${colorClasses[0]} px-2 py-1 w-[calc(100vw-1.5rem)] md:w-[700px] shadow-md hover:shadow-lg`}>
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              {icon}
             </div>
+            <div className={`${icon ? "ml-3" : ""}`}>
+              <p className={`text-sm font-medium ${colorClasses[3]}`} dangerouslySetInnerHTML={{ __html: message }} />
+            </div>
+            <div className="ml-auto pl-3">
+              <div className="absolute top-0.5 right-[-0px] opacity-40 hover:opacity-100">
+                <button
+                  type="button"
+                  className={`inline-flex rounded-md ${colorClasses[0]} p-0 ${colorClasses[4]} ${colorClasses[1]} focus:outline-none ${colorClasses[2]} focus:ring-offset-2`}
+                  onClick={handleClose}
+                >
+                  <span className="sr-only">Dismiss</span>
+                  <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className={`_bg-pink-100 text-center`}
+            onClick={handleClose}
+          >
+              <div className={`_bg-pink-200 w-fit m-auto px-2 font-bold cursor-pointer hover:underline text-sm ${colorClasses[0]} ${colorClasses[4]} ${colorClasses[1]}`}>Got it!</div>
           </div>
         </div>
       </div>
