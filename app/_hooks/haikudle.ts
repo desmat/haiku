e4500705d30830ca5f1fd629b1ff3322816c4f37
Mathieu Desjarlais <github@desmat.ca>
2024-02-15 19:43:55 -0500

    Success msg

--- app/_hooks/haikudle.ts
+++ app/_hooks/haikudle.ts
@@ -59,7 +59,7 @@ const useHaikudle: any = create(devtools((set: any, get: any) => ({
       useAlert.getState().plain(`
         <div style="display: flex; flex-direction: column; gap: 0.4rem">
           <div>Solved in ${moves} move${moves > 1 ? "s" : ""}! <b><a href="https://haikudle.desmat.ca?id=${id}" target="_blank">Share</a></b> on your social networks and come back tomorrow for a new Haiku puzzle.</div>
-          <div>Until then try the generate button above: provide any theme and see what you get!</div>
+          <div>Until then try the generate button above and see what AI comes up with!</div>
         </div>`);
     }, 250);
   },
