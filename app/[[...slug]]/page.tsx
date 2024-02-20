009369d2a7ab216ab77d6c6da9598257bd818509
Mathieu Desjarlais <github@desmat.ca>
2024-02-20 15:58:16 -0500

    .

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -9,10 +9,11 @@ export default function Page({
   params: { slug: any } 
   searchParams?: { [key: string]: string | undefined },
  }) {
-  console.log('>> app.[[...slugs]].page.render()', { slug: params.slug, searchParams });
+  // console.log('>> app.[[...slugs]].page.render()', { slug: params.slug, searchParams });
   const id = searchParams && searchParams["id"] || params.slug && params.slug[0];
   const lang = searchParams && searchParams["lang"] as LanguageType || "en";
   const mode = searchParams &&  searchParams["mode"] || process.env.EXPERIENCE_MODE || "haiku";
+  console.log('>> app.[[...slugs]].page.render()', { slug: params.slug, searchParams, id, lang, mode });
 
   if (!isSupportedLanguage(lang)) {
     return <NotFound mode={mode} />
