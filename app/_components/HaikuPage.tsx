e1c2964df8d12d47d677b227ecbd9954e253c8e7
Mathieu Desjarlais <github@desmat.ca>
2024-02-21 19:09:20 -0500

    better blur curve

--- app/_components/HaikuPage.tsx
+++ app/_components/HaikuPage.tsx
@@ -199,11 +199,14 @@ export default function HaikuPage({ mode, haiku, styles }: { mode: string, haiku
     }
   }
 
-  const blurCurve = [0, 1.5, 2.5, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14, 14, 14, 14, 14, 14];
+  const blurCurve = [0, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10];
   const numWords = inProgress.flat().length;
   let numCorrectWords = inProgress.flat().filter((word: any) => word.correct).length
   // if (numCorrectWords > 0) numCorrectWords = numCorrectWords + 1; // make the last transition more impactful
-  const blurValue = process.env.BACKGROUND_BLUR == "progressive" ? blurCurve[numWords - numCorrectWords] : 0;
+  let blurValue = process.env.BACKGROUND_BLUR == "progressive" ? blurCurve[numWords - numCorrectWords] : 0;
+  if (typeof(blurValue) != "number") {
+    blurValue = blurCurve[blurCurve.length - 1];
+  }
   // console.log('>> app._components.HaikuPage.render()', { numWords, numCorrectWords, blurValue });
 
   return (
