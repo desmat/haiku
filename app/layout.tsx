76503ab701f37bb2ce798bce94a60801d95a860d
Mathieu Desjarlais <github@desmat.ca>
2024-04-29 15:54:59 -0400

    seo placeholders

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,8 @@ import moment from 'moment';
 const isHaikudleMode = process.env.EXPERIENCE_MODE == "haikudle";
 const inter = Inter({ subsets: ['latin'] });
 
-const appName = isHaikudleMode
-  ? "Solve the puzzle to reveal today's haiku, generate and share haiku poems paired and beautiful generative art with Haikudle - AI-powered haiku poetry, puzzles and visual art"
-  : "Compose and share your haiku creations with Haiku Genius - AI-powered haiku poetry and generative art";
-
-const appDescription = isHaikudleMode
-  ? "Solve the daily puzzles, generate new haikus, enjoy beautiful generative art and share with the world - no signup required. " +
-    "Haikudle integrates cutting-edge AI technology to elevate your poetic experience to new heights. " +
-    "Generate haiku poems and share AI-generated creations with stunning AI-generated imagery, powered by OpenAI's ChatGPT and DALL-E. " +
-    "Explore daily haiku puzzles and discover the limitless poetic and artistic possibilities with Haikudle."
-  : "Create and share your haiku masterpieces with beautiful generated art — no signup required. " +
-    "Haiku Genius integrates cutting-edge AI technology to elevate your poetic experience to new heights. " +
-    "Craft haiku poems seamlessly with our AI assistant and share your creations with stunning AI-generated imagery, powered by OpenAI's ChatGPT and DALL-E. " +
-    "Explore daily featured haikus and experience AI-assisted creativity and discover the limitless possibilities of poetic exploration with Haiku Genius.";
-
+const appName = "TODO NAME AND THINGS"
+const appDescription = "TODO DESCRIPTION AND KEYWORDS"
 const metaUrl = "https://limerick.desmat.ca/";
 
 const haikuGeniusMetaImages = [
