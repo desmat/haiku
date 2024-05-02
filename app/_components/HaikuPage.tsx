44f6e24c7f8c080ac2dd8c483e8d3a029991875a
Mathieu Desjarlais <github@desmat.ca>
2024-05-02 12:59:29 -0400

    Funny loading messages

--- app/_components/HaikuPage.tsx
+++ app/_components/HaikuPage.tsx
@@ -56,9 +56,11 @@ export default function HaikuPage({
       />
       <div className={`${font.architects_daughter.className} _bg-yellow-200 md:text-[26pt] sm:text-[22pt] text-[16pt] absolute top-0 left-0 right-0 bottom-0 m-auto w-fit h-fit ${onboardingElement && ["poem", "poem-actions", "poem-and-poem-actions"].includes(onboardingElement) ? "z-50" : "z-10"} _transition-all `}>
         {(regenerating || loading) &&
-          <Loading styles={styles} />
+          <div className="_bg-yellow-200 w-[100vw]">
+            <Loading styles={styles} />
+          </div>
         }
-        {!regenerating && !loading && mode != "social-img" && 
+        {!regenerating && !loading && mode != "social-img" &&
           <div className="_bg-pink-200 relative">
             <HaikuPoem
               user={user}
--- app/_components/Loading.tsx
+++ app/_components/Loading.tsx
@@ -7,8 +7,27 @@ import { StyledLayers } from "./StyledLayers";
 import { default as ClientLoading } from "@/app/_components/client/Loading";
 
 export const loadingMessages = [
-  "Loading",
-]
+  "Tickling Leprachans",
+  "Milking Unicorns",
+  "Separating Green Skittles",
+  "Befriending Ghosts in the Attic",
+  "Arm Wrestling with Mannequins",
+  "Brewing Coffee for Sleepwalkers",
+  "Negotiating with Noisy Neighbors",
+  "Pillow Fighting with Pirates",
+  "Serenading Seahorses",
+  "Training Turtles for the Olympics",
+  "Playing Poker with Parrots",
+  "Crocheting Cozies for Bald Eagles",
+  "Waltzing with Washing Machines",
+  "Squishing Marshmallows on Millionaires",
+  "Sipping Secrets from Soup Spoons",
+  "Gossiping with Goldfishes",
+  "Knitting Sweaters for Snakes",
+  "Babysitting Bandicoots",
+  "Tickling Turtles with Feathers",
+  "Serenading Sandwiches at Midnight",
+];
 
 export default function Loading({ styles = [] }: { styles?: any }) {
   const seed = Math.floor(moment().valueOf() / (5 * 60 * 1000)); // make sure client and server sides render the same within a reasonable window
@@ -21,7 +40,8 @@ export default function Loading({ styles = [] }: { styles?: any }) {
       fallback={
         <Link
           href="/"
-          className={`${font.architects_daughter.className} _bg-pink-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-dark-2 opacity-50 cursor-pointer z-50 md:text-[26pt] sm:text-[22pt] text-[16pt]`}
+          className={`${font.architects_daughter.className} _bg-pink-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full p-2 opacity-50 cursor-pointer z-50 md:text-[26pt] sm:text-[22pt] text-[16pt]`}
+          style={{ textDecoration: "none" }}
         >
           <StyledLayers styles={styles}>
             <div className="animate-pulse flex flex-col items-center">
