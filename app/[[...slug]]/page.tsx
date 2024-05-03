9631622b5903857a90f818ed02075aaec4c4101f
Mathieu Desjarlais <github@desmat.ca>
2024-05-03 12:46:17 -0400

    Testing out dynamic meta og:image

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -2,19 +2,19 @@ import MainPage from '@/app/_components/MainPage';
 import NotFound from '@/app/not-found';
 import { LanguageType, isSupportedLanguage } from '@/types/Languages';
 
-export default function Page({ 
+export default function Page({
   params,
   searchParams,
-}: { 
-  params: { slug: any } 
+}: {
+  params: { slug: any }
   searchParams?: { [key: string]: string | undefined },
- }) {
+}) {
   let id = searchParams && searchParams["id"] || params.slug && params.slug[0];
   const version = searchParams && searchParams["version"];
   const lang = searchParams && searchParams["lang"] as LanguageType || "en";
-  const mode = searchParams &&  searchParams["mode"] || process.env.EXPERIENCE_MODE || "haiku";
+  const mode = searchParams && searchParams["mode"] || process.env.EXPERIENCE_MODE || "haiku";
   const refreshDelay = searchParams && Number(searchParams["refreshDelay"]);
-  // console.log('>> app.[[...slugs]].page.render()', { slug: params.slug, searchParams, id, version, lang, mode });
+  console.log('>> app.[[...slugs]].page.render()', { slug: params.slug, searchParams, id, version, lang, mode });
 
   if (!isSupportedLanguage(lang)) {
     return <NotFound mode={mode} />
@@ -24,6 +24,6 @@ export default function Page({
   if (id == "index") {
     id = undefined;
   }
-  
+
   return <MainPage mode={mode} id={id} version={version} lang={lang} refreshDelay={refreshDelay} />
 }
