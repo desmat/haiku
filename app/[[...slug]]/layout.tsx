eae381710efa8cf7747929ac624256ae9d94f22e
Mathieu Desjarlais <github@desmat.ca>
2024-05-03 13:36:22 -0400

    .

--- app/[[...slug]]/layout.tsx
+++ app/[[...slug]]/layout.tsx
@@ -26,7 +26,7 @@ const appDescription = isHaikudleMode
 
 const metaUrl = isHaikudleMode
   ? "https://haikudle.art/"
-  : "https://haikugenius.io/";
+  : "https://haiku.desmat.ca/";
 
 const haikuGeniusMetaImages = [
   "https://iwpybzbnjyjnfzli.public.blob.vercel-storage.com/social_img_haikugenius/haikugenius_98b222c0_mountains.png",
