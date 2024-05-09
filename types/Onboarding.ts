6d5835f7012ce54fcd93309a84258e868c312978
Mathieu Desjarlais <github@desmat.ca>
2024-05-09 11:03:25 -0400

    created by and updated by in admin info

--- types/Onboarding.ts
+++ types/Onboarding.ts
@@ -1,3 +1,4 @@
+import { formatTimeFromNow } from "@/utils/format";
 import { Haiku } from "./Haiku";
 
 export type OnboardingStep = {
@@ -74,19 +75,21 @@ export const haikuPromptSteps = (haiku: Haiku) => [
     focus: "poem",
     message: `
       <div style="display: flex; flex-direction: column; gap: 0.4rem;">
-      <div>Haiku theme and mood: <i>${haiku.theme}</i> and <i>${haiku.mood}</i></div>      
-      <div>Image style: <i>${haiku.artStyle || "N/A"}</i></div>      
-      <!-- <div>Poem prompt: <i>${haiku.poemPrompt || "N/A"}</i></div> -->
-      <div>Image prompt: <i>${haiku.imagePrompt || "N/A"}</i></div>
-      ${haiku.version || haiku.deprecated
-        ? `<div>Version: ${haiku.version}
-          ${haiku.version
-          ? ` <a href="/${haiku.id}?version=${haiku.version - 1}">(Load previous)</a>`
-          : ""}
-          ${haiku.deprecated
-          ? ` <a href="/${haiku.id}">(Load current)</a>`
-          : ""}</div>`
-        : ""}    
+        <div>Theme and mood: <i>${haiku.theme}</i> and <i>${haiku.mood}</i></div>      
+        <div>Image style: <i>${haiku.artStyle || "N/A"}</i></div>      
+        <!-- <div>Poem prompt: <i>${haiku.poemPrompt || "N/A"}</i></div> -->
+        <div>Image prompt: <i>${haiku.imagePrompt || "N/A"}</i></div>
+        ${haiku.version || haiku.deprecated
+          ? `<div>Version: ${haiku.version}
+            ${haiku.version
+            ? ` <a href="/${haiku.id}?version=${haiku.version - 1}">(Load previous)</a>`
+            : ""}
+            ${haiku.deprecated
+            ? ` <a href="/${haiku.id}">(Load current)</a>`
+            : ""}</div>`
+          : ""}    
+        <div>Created by: user ${haiku.createdBy} ${formatTimeFromNow(haiku.createdAt || 0)}</div>      
+        ${haiku.updatedBy ? `<div>Updated by: user ${haiku.updatedBy} ${formatTimeFromNow(haiku.updatedAt || 0)}</div>` : ""} 
       </div>`,
     style: { bottom: "50%", transform: "translateY(50%)" },
   },
