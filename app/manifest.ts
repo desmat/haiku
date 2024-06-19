1bfbd7907bae3ca7b2bd9ae3f189f2a8e1a921c8
Mathieu Desjarlais <github@desmat.ca>
2024-06-19 12:06:11 -0400

    quick logo

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,16 +1,14 @@
 import { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
-  const isHaikudleMode = process.env.EXPERIENCE_MODE == "haikudle";
-
   return {
-    name: isHaikudleMode ? 'hAIkudle - AI-powered daily Haiku puzzles' : 'hAIku - AI-generated art and Haiku poems',
-    short_name: isHaikudleMode ? 'hAIkudle' : 'hAIku',
-    description: isHaikudleMode ? 'AI-powered daily Haiku puzzles' : 'AI-generated art and Haiku poems',
+    name: 'Limericks - AI-Powered Limerick Poetry and Generative Art',
+    short_name: 'Limericks',
+    description: 'AI-Powered Limerick Poetry and Generative Art',
     start_url: '/',
     display: 'standalone',
-    background_color: 'rgb(32, 31, 27)',
-    theme_color: 'rgb(32, 31, 27)',
+    background_color: '#e5e0c4',
+    theme_color: '#e5e0c4',
     icons: [
       {
         src: 'favicon.ico',
