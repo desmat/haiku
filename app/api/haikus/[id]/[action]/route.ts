9f10f89c55d462dae0ee4b32f850edac6d42722b
Mathieu Desjarlais <github@desmat.ca>
2024-06-20 12:14:35 -0400

    refactor /api/haikus/[id]/regenerate

--- app/api/haikus/[id]/[action]/route.ts
+++ app/api/haikus/[id]/[action]/route.ts
@@ -1,7 +1,9 @@
 import moment from 'moment';
 import { NextResponse } from 'next/server'
-import { getHaiku, getUserHaiku, createUserHaiku, saveUserHaiku } from '@/services/haikus';
+import { getHaiku, getUserHaiku, createUserHaiku, saveUserHaiku, regenerateHaikuImage, regenerateHaikuPoem } from '@/services/haikus';
+import { userUsage } from '@/services/usage';
 import { userSession } from '@/services/users';
+import { USAGE_LIMIT } from '@/types/Usage';
 
 export const maxDuration = 300;
 // export const dynamic = 'force-dynamic';
@@ -36,6 +38,47 @@ export async function POST(
     });
 
     return NextResponse.json({ haiku, userHaiku: savedUserHaiku });
+  } else if (params.action == "regenerate") {
+    let { haiku, part, artStyle }: any = await request.json();
+    part = part || "poem";
+  
+    console.log(`>> app.api.haiku.[id].[action].POST`, { action: params.action, haiku, part });
+
+    const { user } = await userSession(request);
+    let reachedUsageLimit = false; // actually _will_ reach usage limit shortly
+  
+    if (!user.isAdmin) {
+      const h = await getHaiku(user, haiku.id);
+      
+      // only owners and admins can update
+      if (!user.isAdmin && h.createdBy != haiku.createdBy) {
+        return NextResponse.json(
+          { success: false, message: 'authorization failed' },
+          { status: 403 }
+        );  
+      }
+  
+      const usage = await userUsage(user);
+      const { haikusRegenerated } = usage[moment().format("YYYYMMDD")];
+      console.log('>> app.api.haiku.regenerate.POST', { haikusRegenerated, usage });
+  
+      if ((haikusRegenerated || 0) >= USAGE_LIMIT.DAILY_REGENERATE_HAIKU) {
+        return NextResponse.json(
+          { success: false, message: 'exceeded daily limit' },
+          { status: 429 }
+        );
+      } else if ((haikusRegenerated || 0) + 1 == USAGE_LIMIT.DAILY_REGENERATE_HAIKU) {
+        reachedUsageLimit = true;
+      }
+    }
+  
+    if (!["image", "poem"].includes(part))throw `Regenerate part not supported: ${part}`;
+  
+    const updatedHaiku = part == "image"
+      ? await regenerateHaikuImage(user, haiku, artStyle)
+      : await regenerateHaikuPoem(user, haiku);
+        
+    return NextResponse.json({ haiku: updatedHaiku, reachedUsageLimit });
   } else {
     return NextResponse.json(
       { success: false, message: 'unsupported action' },
