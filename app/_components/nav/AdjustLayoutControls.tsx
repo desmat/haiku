55e52a0b585dfe0a8cb910b6578f1ef8f416d9cf
Mathieu Desjarlais <github@desmat.ca>
2024-06-26 10:46:39 -0400

    rm debug bg-colors

--- app/_components/nav/AdjustLayoutControls.tsx
+++ app/_components/nav/AdjustLayoutControls.tsx
@@ -22,7 +22,7 @@ export default function AdjustLayoutControls({
   return (
     <div className="adjust-layout-controls">
       <div
-        className="bg-blue-400 w-[40vw] h-40 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] fixed cursor-move z-20"
+        className="_bg-blue-400 w-[40vw] h-40 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] fixed cursor-move z-20"
         onClick={(e: any) => {
           adjustLayoutClamped({ poem: {} });
           e.preventDefault();
@@ -30,7 +30,7 @@ export default function AdjustLayoutControls({
         title="Center poem"
       />
       <div
-        className="bg-pink-200 w-[40vw] h-10 left-[50%] translate-x-[-50%] -top-10 absolute cursor-n-resize z-30"
+        className="_bg-pink-200 w-[40vw] h-10 left-[50%] translate-x-[-50%] -top-10 absolute cursor-n-resize z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             top: poemLayout?.top ? poemLayout.top - 5 : undefined,
@@ -42,7 +42,7 @@ export default function AdjustLayoutControls({
         title="Move poem up slightly"
       />
       <div
-        className="bg-pink-400 w-[40vw] h-10 left-[50%] translate-x-[-50%] -top-20 absolute cursor-n-resize z-30"
+        className="_bg-pink-400 w-[40vw] h-10 left-[50%] translate-x-[-50%] -top-20 absolute cursor-n-resize z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             top: poemLayout?.top ? poemLayout.top - 15 : undefined,
@@ -54,7 +54,7 @@ export default function AdjustLayoutControls({
         title="Move poem up"
       />
       <div
-        className="bg-pink-200 w-[40vw] h-10 left-[50%] translate-x-[-50%] -bottom-10 absolute cursor-s-resize z-30"
+        className="_bg-pink-200 w-[40vw] h-10 left-[50%] translate-x-[-50%] -bottom-10 absolute cursor-s-resize z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             top: poemLayout?.top ? poemLayout.top + 5 : undefined,
@@ -66,7 +66,7 @@ export default function AdjustLayoutControls({
         title="Move poem down slightly"
       />
       <div
-        className="bg-pink-400 w-[40vw] h-10 left-[50%] translate-x-[-50%] -bottom-20 absolute cursor-s-resize z-30"
+        className="_bg-pink-400 w-[40vw] h-10 left-[50%] translate-x-[-50%] -bottom-20 absolute cursor-s-resize z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             top: poemLayout?.top ? poemLayout.top + 15 : undefined,
@@ -78,7 +78,7 @@ export default function AdjustLayoutControls({
         title="Move poem down"
       />
       <div
-        className="bg-blue-200 w-[40vw] h-[3rem] left-[50%] translate-x-[-50%] top-0 fixed cursor-move z-30"
+        className="_bg-blue-200 w-[40vw] h-[3rem] left-[50%] translate-x-[-50%] top-0 fixed cursor-move z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             top: poemLayout?.top ? 5 : 15,
@@ -87,7 +87,7 @@ export default function AdjustLayoutControls({
         title="Move poem to top"
       />
       <div
-        className="bg-blue-200 w-[40vw] h-[3rem] left-[50%] translate-x-[-50%] bottom-0 fixed cursor-move z-30"
+        className="_bg-blue-200 w-[40vw] h-[3rem] left-[50%] translate-x-[-50%] bottom-0 fixed cursor-move z-30"
         onClick={(e: any) => {
           adjustLayoutClamped({
             bottom: poemLayout?.bottom ? 5 : 15
