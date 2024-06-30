ac9c040797f19bf4177b9a80982620a04d847773
Mathieu Desjarlais <github@desmat.ca>
2024-06-30 08:58:14 -0400

    facebook-domain-verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="canonical" href={metaUrl} key="canonical" />
         <meta property="fb:app_id" content={process.env.FB_APP_ID}></meta>
+        <meta name="facebook-domain-verification" content="db7teiurqqk7esipbaxtnklep23oy9" />
       </head>
       <body
         className={inter.className}
