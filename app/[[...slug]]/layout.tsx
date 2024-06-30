4772db4509296e24893c77c351eb3ab615d9ad41
Mathieu Desjarlais <github@desmat.ca>
2024-06-30 10:24:55 -0400

    rm debug

--- app/[[...slug]]/layout.tsx
+++ app/[[...slug]]/layout.tsx
@@ -101,7 +101,7 @@ export default async function Layout({
     }
   }
 
-  console.log('>> app.[[..slug]].layout.render()', { metadata, images: JSON.stringify(metadata.openGraph?.images) });
+  // console.log('>> app.[[..slug]].layout.render()', { metadata, images: JSON.stringify(metadata.openGraph?.images) });
 
   return (
     <section>
