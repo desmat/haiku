74f4cb36ef05ea57283e78124ea762cbde0f0e0d
Mathieu Desjarlais <github@desmat.ca>
2024-07-06 14:49:29 -0400

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import moment from 'moment';
+import { MetadataRoute } from 'next'
+import { getDailyHaiku } from '@/services/haikus';
+import { metaUrl } from './layout';
+ 
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const latestDailyHaiku = await getDailyHaiku();
+  const lastModified = moment(latestDailyHaiku?.id).toDate();
+
+  return [
+    {
+      url: metaUrl,
+      lastModified,
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
