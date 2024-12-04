8f23691754efc82c0dba4fc9d14cb06dd0dade78
Mathieu Desjarlais <github@desmat.ca>
2024-12-04 15:25:53 -0500

    facebookexternalhit

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,19 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/api/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+        disallow: '/api/',
+      },
+    ],
     sitemap: 'https://haikugenius.ai/sitemap.xml',
   }
 }
