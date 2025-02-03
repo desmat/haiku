9ec08b3d32815c3e9127509dc22915c5ff638d8b
Mathieu Desjarlais <github@desmat.ca>
2025-02-03 18:03:18 -0500

    troubleshoot build issue

--- app/api/haikus/[id]/[action]/route.ts
+++ app/api/haikus/[id]/[action]/route.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import { NextResponse } from 'next/server'
-import { getHaiku, saveUserHaiku, regenerateHaikuImage, regenerateHaikuPoem, updateHaikuImage, likeHaiku, flagHaiku, saveHaiku, getUserHaiku, createUserHaiku, addToAlbum, } from '@/services/haikus';
+import { getHaiku, saveUserHaiku, regenerateHaikuImage, regenerateHaikuPoem, updateHaikuImage, likeHaiku, flagHaiku, saveHaiku, getUserHaiku, createUserHaiku, addToAlbum, updateLayout, } from '@/services/haikus';
 import { userUsage } from '@/services/usage';
 import { userSession } from '@/services/users';
 import { triggerHaikuShared } from '@/services/webhooks';
@@ -212,36 +212,36 @@ export async function POST(
     }
 
     return NextResponse.json({ haiku });
-  // } else if (params.action == "updateLayout") {
-  //   const [{ value: album }, { user }] = await Promise.all([
-  //     request.json(),
-  //     userSession(request),
-  //   ]);
+  } else if (params.action == "updateLayout") {
+    const [{ value: album }, { user }] = await Promise.all([
+      request.json(),
+      userSession(request),
+    ]);
 
-  //   // only admins can update the image directly
-  //   if (!user.isAdmin) {
-  //     return NextResponse.json(
-  //       { success: false, message: 'authorization failed' },
-  //       { status: 403 }
-  //     );
-  //   }
+    // only admins can update the image directly
+    if (!user.isAdmin) {
+      return NextResponse.json(
+        { success: false, message: 'authorization failed' },
+        { status: 403 }
+      );
+    }
 
-  //   let haiku = await getHaiku(user, params.id)
+    let haiku = await getHaiku(user, params.id)
 
-  //   if (!haiku) {
-  //     return NextResponse.json(
-  //       { success: false, message: 'haiku not found' },
-  //       { status: 404 }
-  //     );
-  //   }
+    if (!haiku) {
+      return NextResponse.json(
+        { success: false, message: 'haiku not found' },
+        { status: 404 }
+      );
+    }
 
-  //   haiku = await saveHaiku(
-  //     user, 
-  //     await updateLayout(user, haiku),
-  //     { noVersion: true }
-  //   );
+    haiku = await saveHaiku(
+      user, 
+      await updateLayout(user, haiku),
+      { noVersion: true }
+    );
 
-  //   return NextResponse.json({ haiku });
+    return NextResponse.json({ haiku });
   } else if (params.action == "addToAlbum") {
     const [{ value: album }, { user }] = await Promise.all([
       request.json(),
