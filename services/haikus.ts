56f2d0633e4de8acd3c1e7ac3a19cf17a2014a10
Mathieu Desjarlais <github@desmat.ca>
2025-04-30 08:03:42 -0400

    un-break showcase title/subject

--- services/haikus.ts
+++ services/haikus.ts
@@ -1385,9 +1385,6 @@ function cleanupHaikuForNonAdmins(haiku: Haiku) {
   delete haiku.poemPrompt;
   delete haiku.sharedVersioned;
   delete haiku.status;
-  // delete haiku.subject;
-  // delete haiku.theme;
-  //delete haiku.title;
 
   return haiku;
 }
